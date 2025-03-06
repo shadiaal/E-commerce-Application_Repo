79962d16d69975385f6c4faa95c3b895c2e28658
shadiaal <shadiaalmtiri@gmail.com>
2025-03-06 22:47:04 +0300

    Added all cart-related code backend functionality

---
+++ BackEnd/EcommerceBackend/CartItem.cs
@@ -0,0 +1,9 @@
+namespace EcommerceBackend.Models{
+  public class CartItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }= string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
