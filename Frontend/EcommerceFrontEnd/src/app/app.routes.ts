297a5a07666968361c8d33b24d1772c298b7756a
shadiaal <shadiaalmtiri@gmail.com>
2025-03-06 22:38:44 +0300

    Added cart operations to API

--- Frontend/EcommerceFrontEnd/src/app/app.routes.ts
+++ Frontend/EcommerceFrontEnd/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CartComponent } from './components/cart/cart.component';
 
 export const routes: Routes = [
 
     { path: 'products', component: ProductListComponent },
     { path: '', redirectTo: '/products', pathMatch: 'full' },
+    { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
--- Frontend/EcommerceFrontEnd/src/app/services/product.service.ts
+++ Frontend/EcommerceFrontEnd/src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 // src/app/services/product.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient,HttpErrorResponse  } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,31 @@ export class ProductService {
 
   constructor(private http: HttpClient) {}
 
+  // Get all products
   getProducts(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get product details by ID
+  getProductById(id: string): Observable<any> {
+    const numericId = +id; // Convert string to number
+    return this.http.get<any>(`${this.apiUrl}/${numericId}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+  
+
+   // Handle errors
+   private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An error occurred while communicating with the server';
+    if (error.status === 404) {
+      errorMessage = 'Product not found';
+    } else if (error.status === 500) {
+      errorMessage = 'There is a problem with the server, please try again later';
+    }
+    alert(errorMessage);
+    return throwError(() => new Error(errorMessage));
   }
 }
