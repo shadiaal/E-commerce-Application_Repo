45bb1544d8ce7bf3c34ffd2a07354c6b88f85a21
shadiaal <shadiaalmtiri@gmail.com>
2025-03-06 17:33:00 +0300

    Add Product_List_Page

--- BackEnd/EcommerceBackend/Program.cs
+++ BackEnd/EcommerceBackend/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options=>{
 options.AddPolicy("AllowAngularApp",policy=>
-policy.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader());
+policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
 
 });
 var app = builder.Build();
